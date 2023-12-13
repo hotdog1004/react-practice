b502667138c6246ed794f271ab0c5679c5e64719
hotdog1004 <hotdog87675210@gmail.com>
2023-12-13 14:07:43 +0900

    [chore] lint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [
     {
       env: {
