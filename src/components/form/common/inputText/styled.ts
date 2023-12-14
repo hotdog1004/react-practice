f4b09f992e3e5ff8251d910dd875c336fe38d233
hotdog1004 <hotdog87675210@gmail.com>
2023-12-14 18:27:19 +0900

    [feat] InputText style 파일 추가

---
+++ src/components/form/common/inputText/styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { TextField } from '@mui/material';
+
+export const CustomTextField = styled(TextField)({
+  '& .MuiInputBase-input': {
+    padding: '0.375rem 0.75rem',
+  },
+});
